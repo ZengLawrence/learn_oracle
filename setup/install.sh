1bfcff73d991637343fb496a2431da4f1cacc07e
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-01-21 11:21:31 -0500

    install using Docker

---
+++ setup/install.sh
@@ -0,0 +1,10 @@
+# installation instruction can be found
+# https://container-registry.oracle.com/ords/f?p=113:4:13406972935626:::4:P4_REPOSITORY,AI_REPOSITORY,AI_REPOSITORY_NAME,P4_REPOSITORY_NAME,P4_EULA_ID,P4_BUSINESS_AREA_ID:1863,1863,Oracle%20Database%20Free,Oracle%20Database%20Free,1,0&cs=3QmhM7uHXwrNo0J_3uhUh7LUQSdb9Lhc28Sby0RvF83r5Obpzt6t9fdnlM4X5pVXBhWuuee95VPJ4xLMueb-ktA
+
+# Modify for Oracle Free version
+
+docker exec oracle-23c ./setPassword.sh foo
+
+docker exec -it oracle-23c sqlplus sys/foo@FREE as sysdba
+docker exec -it oracle-23c sqlplus system/foo@FREE
+docker exec -it oracle-23c sqlplus pdbadmin/foo@FREEPDB1
