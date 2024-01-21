beb7675e0caf9ebf3f2c938c90255f50cb0e0f62
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-01-21 15:00:12 -0500

    db user connection

--- setup/connect.sh
+++ setup/connect.sh
@@ -1,3 +1,5 @@
 docker exec -it oracle-23c sqlplus sys/foo@FREE as sysdba
 docker exec -it oracle-23c sqlplus system/foo@FREE
 docker exec -it oracle-23c sqlplus pdbadmin/foo@FREEPDB1
+
+sqlplus scott/tiger@localhost:1521/FREEPDB1
